a17f48a4ff92112c6fdc3ce1d04b9474551af379
David Jones <drj@pobox.com>
2013-09-07 20:40:12 +0100

    Some common functions.

---
+++ setup.go
@@ -0,0 +1,28 @@
+package gohm
+
+import (
+  "errors"
+  "path/filepath"
+  "log"
+  "os"
+)
+
+func Setup() (string, error) {
+	GOHM_PATH := os.Getenv("GOHM_PATH")
+
+	if GOHM_PATH == "" {
+		return "", errors.New("GOHM_PATH must be set")
+	}
+	_ = os.MkdirAll(GOHM_PATH, 0777)
+
+	// Logging
+	l := filepath.Join(GOHM_PATH, "inc.log")
+	logf, err := os.OpenFile(l, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	if err != nil {
+		log.Println(err.Error())
+		log.Println("Logging to stderr instead.")
+	} else {
+		log.SetOutput(logf)
+	}
+        return GOHM_PATH, nil
+}
