2b13fa4ab106048b5b8d203e1ca280e621c13f45
David Jones <drj@pobox.com>
2013-09-13 14:01:28 +0100

    Avoid defer in loop

--- scan/main.go
+++ scan/main.go
@@ -11,6 +11,7 @@ import (
 
 var (
 	_, setupErr = gohm.Setup()
+	current     string
 )
 
 func main() {
@@ -32,42 +33,46 @@ func mainExitStatus() int {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 	}
-	current := gohm.CurrentMessage()
+	current = gohm.CurrentMessage()
 	for _, f := range entries {
-		// Flags for current message and replied.
-		// :todo:(drj) implement.
-		var curr, repl string
 		var i_ int
 		n, _ := fmt.Sscan(f.Name(), &i_)
-		if current == f.Name() {
-			curr = "+"
-		}
 		if n == 1 {
-			fullName := filepath.Join(dir, f.Name())
-			fp, err := os.Open(fullName)
-			if err != nil {
-				panic(err.Error())
-			}
-			defer fp.Close()
-			msg, _ := mail.ReadMessage(fp)
-			var subj = "??"
-			var date = "MM-DD"
-			var from = "From?"
-			if msg != nil {
-				subj = msg.Header.Get("Subject")
-				t, err := msg.Header.Date()
-				if err == nil {
-					date = t.Format("01-02")
-				}
-				froms, err := msg.Header.AddressList("From")
-				if froms != nil {
-					from = froms[0].Name
-				}
-			}
-			// Format taken from example on
-			// http://rand-mh.sourceforge.net/book/mh/faswsprs.html
-			fmt.Printf("%4s%1s%1s%s %-17.17s  %-40.40s\n", f.Name(), curr, repl, date, from, subj)
+			scan1(dir, f.Name())
 		}
 	}
 	return 0
+}
+
+func scan1(dir, name string) {
+	fullName := filepath.Join(dir, name)
+	// Flags for current message and replied.
+	// :todo:(drj) implement.
+	var curr, repl string
+	if current == name {
+		curr = "+"
+	}
+	fp, err := os.Open(fullName)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer fp.Close()
+	msg, _ := mail.ReadMessage(fp)
+	var subj = "??"
+	var date = "MM-DD"
+	var from = "From?"
+	if msg != nil {
+		subj = msg.Header.Get("Subject")
+		t, err := msg.Header.Date()
+		if err == nil {
+			date = t.Format("01-02")
+		}
+		froms, err := msg.Header.AddressList("From")
+		if froms != nil {
+			from = froms[0].Name
+		}
+	}
+	// Format taken from example on
+	// http://rand-mh.sourceforge.net/book/mh/faswsprs.html
+	fmt.Printf("%4s%1s%1s%s %-17.17s  %-40.40s\n", name, curr, repl, date, from, subj)
 }
